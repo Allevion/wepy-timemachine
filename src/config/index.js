cc3144138c51b76d778395f59a75ef0c00c420ea
Allevion <an10321@sina.com>
2019-04-01 16:23:41 +0800

    理财师小助手

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,4 @@
-// export const baseUrl = 'https://raw.githubusercontent.com/BelinChung/api-mock/master/HiApp'
-// export const baseUrl = 'https://appserver.88mmmoney.com/appServer/'
-export const baseUrl = 'https://appserver.mmzb.com/appServer/'
+export const baseUrl = 'https://appserver.88mmmoney.com/appServer/'
+// export const baseUrl = 'https://appserver.mmzb.com/appServer/'
 // export const baseUrl = 'http://192.168.10.92:8480/appServer/'
-// export const baseUrl = 'http://appserver.mmzb.com/appServer/'
 // export const baseUrl = 'http://192.168.10.164:8480/app-wap-personal/'
